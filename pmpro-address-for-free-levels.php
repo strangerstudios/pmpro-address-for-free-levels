bc93c2f5da8620d3d68881035e623c36c6c49089
Jason Coleman <jason@strangerstudios.com>
2017-02-06 12:26:26 -0500

    ...

---
+++ pmpro-address-for-free-levels.php
@@ -0,0 +1,183 @@
+<?php
+/*
+Plugin Name: Paid Memberships Pro - Address For Free Levels Add On 
+Plugin URI: http://www.paidmembershipspro.com/wp/pmpro-address-for-free-levels/
+Description: Show address fields for free levels also with Paid Memberships Pro
+Version: .3.2
+Author: Stranger Studios
+Author URI: http://www.strangerstudios.com
+*/
+ 
+/*
+  Show address fields for free levels too.
+  Add this code to your active theme's functions.php or a custom plugin.
+*/
+//keep address fields shown on checkout page
+function pmproaffl_pmpro_checkout_boxes_require_address()
+{
+	//don't force show them on review page
+	global $pmpro_review;		
+?>
+<script>
+	var pmpro_show_billing_address_fields_timer;
+	function showBillingAddressFields()
+	{
+		<?php if(empty($pmpro_review)) { ?>
+			jQuery('#pmpro_billing_address_fields').show();
+		<?php } else { ?>
+			jQuery('#pmpro_billing_address_fields').hide();	//hiding when on the review page
+		<?php } ?>
+		
+		pmpro_show_billing_address_fields_timer = setTimeout(function(){showBillingAddressFields();}, 200);
+	}
+	jQuery(document).ready(function() {
+		//show it and keep showing it
+		showBillingAddressFields();
+		
+		<?php
+			//remove billing from address title if the level is free
+			global $pmpro_level;
+			if(pmpro_isLevelFree($pmpro_level))
+			{
+			?>
+				//change heading
+				jQuery('#pmpro_billing_address_fields th').html('Address');
+			<?php
+			}
+		?>
+	});
+</script>
+<?php
+}
+add_action("pmpro_checkout_boxes", "pmproaffl_pmpro_checkout_boxes_require_address");
+
+//make sure we include address fields (for post 1.8)
+function pmproaffl_init_include_address_fields_at_checkout()
+{
+	add_filter('pmpro_include_billing_address_fields', '__return_true');
+}
+add_action('init', 'pmproaffl_init_include_address_fields_at_checkout', 30);
+ 
+//make sure address fields are required
+function pmproaffl_pmpro_required_billing_fields($fields)
+{
+	global $bfirstname, $blastname, $baddress1, $bcity, $bstate, $bzipcode, $bcountry, $bphone, $bemail;
+
+	$fields["bfirstname"] = $bfirstname;
+	$fields["blastname"] = $blastname;
+	$fields["baddress1"] = $baddress1;
+	$fields["bcity"] = $bcity;
+	$fields["bstate"] = $bstate;
+	$fields["bzipcode"] = $bzipcode;
+	$fields["bphone"] = $bphone;
+	$fields["bemail"] = $bemail;
+	$fields["bcountry"] = $bcountry;
+
+	return $fields;
+}
+//add_action("pmpro_required_user_fields", "pmproaffl_pmpro_required_billing_fields");
+add_action("pmpro_required_billing_fields", "pmproaffl_pmpro_required_billing_fields", 30);
+
+//save fields in session for PayPal Express/etc
+function pmproaffl_pmpro_paypalexpress_session_vars()
+{	
+	//assume the request is set
+	$_SESSION['bfirstname'] = $_REQUEST['bfirstname'];
+	$_SESSION['blastname'] = $_REQUEST['blastname'];
+    $_SESSION['baddress1'] = $_REQUEST['baddress1'];	
+    $_SESSION['bcity'] = $_REQUEST['bcity'];
+    $_SESSION['bstate'] = $_REQUEST['bstate'];
+	$_SESSION['bzipcode'] = $_REQUEST['bzipcode'];
+	$_SESSION['bphone'] = $_REQUEST['bphone'];		
+	$_SESSION['bemail'] = $_REQUEST['bemail'];		
+	$_SESSION['bcountry'] = $_REQUEST['bcountry'];		
+	
+	//check this one cause it's optional
+	if(!empty($_REQUEST['baddress2']))
+		$_SESSION['baddress2'] = $_REQUEST['baddress2'];
+	else
+		$_SESSION['baddress2'] = "";
+		
+	//if there is a user here, save in user meta as well
+	global $current_user;
+	if(!empty($current_user->ID))
+	{
+		//save billing info ect, as user meta
+		$meta_keys = array("pmpro_bfirstname", "pmpro_blastname", "pmpro_baddress1", "pmpro_baddress2", "pmpro_bcity", "pmpro_bstate", "pmpro_bzipcode", "pmpro_bcountry", "pmpro_bphone", "pmpro_bemail");
+		$meta_values = array($_REQUEST['bfirstname'], $_REQUEST['blastname'], $_REQUEST['baddress1'], $_REQUEST['baddress2'], $_REQUEST['bcity'], $_REQUEST['bstate'], $_REQUEST['bzipcode'], $_REQUEST['bcountry'], $_REQUEST['bphone'], $_REQUEST['bemail']);
+		pmpro_replaceUserMeta($current_user->ID, $meta_keys, $meta_values);
+	}
+}
+add_action("pmpro_paypalexpress_session_vars", "pmproaffl_pmpro_paypalexpress_session_vars");
+add_action("pmpro_before_send_to_twocheckout", "pmproaffl_pmpro_paypalexpress_session_vars", 10, 2);
+
+//Load fields from session if available.
+function pmproaffl_init_load_session_vars($param)
+{
+	//check that no field values were passed in and that we have some in session
+	if(empty($_REQUEST['bfirstname']) && !empty($_SESSION['bfirstname']))
+	{
+		$_REQUEST['bfirstname'] = $_SESSION['bfirstname'];
+		$_REQUEST['blastname'] = $_SESSION['blastname'];
+		$_REQUEST['baddress1'] = $_SESSION['baddress1'];
+		$_REQUEST['baddress2'] = $_SESSION['baddress2'];
+		$_REQUEST['bcity'] = $_SESSION['bcity'];
+		$_REQUEST['bstate'] = $_SESSION['bstate'];
+		$_REQUEST['bzipcode'] = $_SESSION['bzipcode'];
+		$_REQUEST['bphone'] = $_SESSION['bphone'];
+		$_REQUEST['bemail'] = $_SESSION['bemail'];
+		$_REQUEST['bcountry'] = $_SESSION['bcountry'];		
+	}
+	
+	return $param;
+}
+add_action('init', 'pmproaffl_init_load_session_vars', 5);
+
+function pmproaffl_pmpro_checkout_order_free($morder)
+{
+	if(empty($morder))
+		$morder = new MemberOrder();
+
+	if(empty($morder->billing))
+		$morder->billing = new stdClass();
+
+	$morder->billing->name = $_REQUEST['bfirstname'] . " " . $_REQUEST['blastname'];
+	$morder->billing->street = $_REQUEST['baddress1'] . " " . $_REQUEST['baddress2'];
+	$morder->billing->city = $_REQUEST['bcity'];
+	$morder->billing->state = $_REQUEST['bstate'];
+	$morder->billing->zip = $_REQUEST['bzipcode'];
+	$morder->billing->phone = $_REQUEST['bphone'];
+	$morder->billing->country= $_REQUEST['bcountry'];	
+	
+	return $morder;
+}
+
+add_filter('pmpro_checkout_order_free', 'pmproaffl_pmpro_checkout_order_free');
+
+//after checkout, clear any session vars
+function pmproaffl_pmpro_after_checkout()
+{
+	$vars = array('bfirstname', 'blastname', 'baddress1', 'baddress2', 'bcity', 'bstate', 'bzipcode', 'bphone', 'bemail', 'bcountry');
+	foreach($vars as $var)
+	{
+		if(isset($_SESSION[$var]))
+			unset($_SESSION[$var]);
+	}	
+}
+add_action("pmpro_after_checkout", "pmproaffl_pmpro_after_checkout");
+
+/*
+Function to add links to the plugin row meta
+*/
+function pmproaffl_plugin_row_meta($links, $file) {
+	if(strpos($file, 'pmpro-address-for-free-levels.php') !== false)
+	{
+		$new_links = array(
+			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/code-gists/pmpro-require-nameaddress-for-free-level/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+		);
+		$links = array_merge($links, $new_links);
+	}
+	return $links;
+}
+add_filter('plugin_row_meta', 'pmproaffl_plugin_row_meta', 10, 2);	
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Paid Memberships Pro - Address For Free Levels Add On ===
+Contributors: strangerstudios
+Tags: pmpro, paid memberships pro, ecommerce
+Requires at least: 3.5
+Tested up to: 4.5.2
+Stable tag: .3.2
+
+Show address fields for free levels also with Paid Memberships Pro
+
+== Description ==
+
+Show address fields for free levels also with Paid Memberships Pro
+
+== Installation ==
+
+1. Upload the `pmpro-address-for-free-levels` directory to the `/wp-content/plugins/` directory of your site.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= I found a bug in the plugin. =
+
+Please post it in the issues section of GitHub and we'll fix it as soon as we can. Thanks for helping. https://github.com/strangerstudios/pmpro-address-for-free-levels/issues
+
+= I need help installing, configuring, or customizing the plugin. =
+
+Please visit our premium support site at http://www.paidmembershipspro.com for more documentation and our support forums.
+
+Please Note: This plugin is meant as a temporary solution. Most updates and fixes will be reserved for when this functionality is built into Paid Memberships Pro. We may not fix the pmpro-addon-packages plugin itself unless it is critical.
+
+== Changelog ==
+= .3.2 =
+* BUG: Fixed a warning.
+
+= .3.1 =
+* BUG: Now filtering required billing fields after gateway actions.
+* ENHANCEMENT: Now using pmpro_require_billing_fields filter instead of pmpro_required_user_fields which doesn't require fields for existing members.
+* BUG: Now storing billing fields in session for Twocheckout as well.
+
+= .3 =
+* BUG: Forcing billing address fields to be included for PayPal Express, PayPal Standard, and Twocheckout. Since PMPro v1.8 these fields are not rendered for these gateways. (Thanks, many folks.)
+* ENHANCEMENT: Hiding the billing fields on the review page if using PayPal Express, PayPal Standard, or Twocheckout.
+
+= .2.1 =
+* BUG: Fixed issue where existing users checking out via PayPal Express wouldn't have their data updated.
+* BUG: Avoiding potential warnings when unsetting SESSION vars.
+
+= .2 =
+* Added support for PayPal Express and friends by storing address fields in $_SESSION.
+
+= .1 =
+* Initial release.
